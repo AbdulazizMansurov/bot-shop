09f7ec49522004f923db1d66b8d3d17b2bf7cc8c
AbdulazizMansurov <abmansurov206@gmail.com>
2022-10-24 16:22:57 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+BU YERDA SIZGA KERAKLI BO'LADIGAN TOKEN VA SHUNGA O'XSHAGAN MA'LUMOTLAR SAQLANADI
+"""
+TOKEN = '5717203628:AAFltyq2vRH_8__C_OvQ25b-lVaOBmZevYw'
+
+PROVIDER_TOKEN = "371317599:TEST:1666262183118"
+ADMINS = '''Adminlarni saqlash uchun o'zgaruvchi, listdaham saqlasa bo'ladi'''
+
+DB_NAME = '''da3jmt34blgqo1'''
+DB_HOST = '''ec2-99-81-16-126.eu-west-1.compute.amazonaws.com'''
+DB_PASSWORD = '''499a0c6c431cec78a3afe4f92f2b68cd4df931e785ff7b8dd2730c68d4a7b42e'''
+DB_PORT = '''5432'''
+DB_USER = '''bsydhmtlooujbf'''
